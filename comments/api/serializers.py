2860d4dc6b242e533e1ccbfb2573049e01af131b
Daixi <dshen@ucsd.edu>
2024-08-23 00:49:37 +0800

    Add comments model api and tests

---
+++ comments/api/serializers.py
@@ -0,0 +1,50 @@
+from accounts.api.serializers import UserSerializerForComment
+from comments.models import Comment
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from tweets.models import Tweet
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializerForComment()
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'tweet_id', 'user', 'content', 'created_at',)
+
+
+class CommentSerializerForCreate(serializers.ModelSerializer):
+    tweet_id = serializers.IntegerField()
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Comment
+        fields = ('content', 'tweet_id', 'user_id',)
+
+    def validate(self, data):
+        tweet_id = data['tweet_id']
+        if not Tweet.objects.filter(id=tweet_id).exists():
+            raise ValidationError({'tweet_id': 'Tweet does not exist'})
+
+        return data
+
+    def create(self, validated_data):
+        # you can create django database object with primary key
+        # instead of instance itself
+        return Comment.objects.create(
+            user_id = validated_data['user_id'],
+            tweet_id = validated_data['tweet_id'],
+            content = validated_data['content'],
+        )
+
+class CommentSerializerForUpdate(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('content',)
+
+    # if instance is passed into serializer,
+    # update will be called
+    def update(self, instance, validated_data):
+        instance.content = validated_data['content']
+        instance.save()
+        return instance
