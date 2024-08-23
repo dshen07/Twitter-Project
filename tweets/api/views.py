5d8c7c477d3b2a9784eb0fdc08969e79776d0d20
Daixi <dshen@ucsd.edu>
2024-08-23 17:01:28 +0800

    Add tweet retrieve method and tests

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,26 +1,34 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from tweets.api.serializers import TweetSerializer, TweetCreateSerializer
+from tweets.api.serializers import TweetSerializer, TweetCreateSerializer, TweetSerializerWithComments
 from tweets.models import Tweet
 from newsfeeds.services import NewsFeedService
+from utils.decorators import required_params
 
 class TweetViewSet(viewsets.GenericViewSet,
                    viewsets.mixins.ListModelMixin,
                    viewsets.mixins.CreateModelMixin):
     serializer_class = TweetCreateSerializer
+    queryset = Tweet.objects.all()
     def get_permissions(self):
-        if self.action == 'list':
+        if self.action in ['list', 'retrieve']:
             return [AllowAny()]
         return [IsAuthenticated()]
+
+    @required_params(params=['user_id'])
     def list(self, request, *args, **kwargs):
-        if 'user_id' not in request.query_params:
-            return Response('missing user_id', status=400)
+        # if 'user_id' not in request.query_params:
+        #     return Response('missing user_id', status=400)
         tweets = Tweet.objects.filter(
             user_id=request.query_params['user_id']
         ).order_by('-created_at')
         serializer = TweetSerializer(tweets, many=True)
         return Response({'tweets': serializer.data})
+
+    def retrieve(self, request, *args, **kwargs):
+        tweet = self.get_object()
+        return Response(TweetSerializerWithComments(tweet).data)
 
     def create(self, request, *args, **kwargs):
         serializer = TweetCreateSerializer(
