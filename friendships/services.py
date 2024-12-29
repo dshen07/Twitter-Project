7939536352cd3fd63af163bd1214048242420f15
Daixi <dshen@ucsd.edu>
2024-12-29 03:17:05 -0500

    fixed minor issue in friendship service

--- friendships/services.py
+++ friendships/services.py
@@ -25,12 +25,6 @@ class FriendshipService(object):
             friendships = HBaseFollower.filter(prefix=(to_user_id, None))
         return [friendship.from_user_id for friendship in friendships]
 
-    @classmethod
-    def has_followed(cls, from_user, to_user):
-        return Friendship.objects.filter(
-            from_user=from_user,
-            to_user=to_user,
-        ).exists()
 
     @classmethod
     def get_following_user_id_set(cls, from_user_id):
